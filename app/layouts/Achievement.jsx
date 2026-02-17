6e0f0260f73fb80b3be1bbb527ca6357020c8c29
Sijan666 <666majharulislam@gmail.com>
2026-02-18 01:32:45 +0600

    feat : add achievement section and used slot counter npm

--- app/layouts/Achievement.jsx
+++ app/layouts/Achievement.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import Container from '../[components]/Container'
+import Flex from '../[components]/Flex'
+import SlotCounter from 'react-slot-counter';
+
 
 const Achievement = () => {
     return (
         <>
         <section className='py-25'>
-            
+            <Container>
+                <h3 className='text-[48px] font-bold text-[#1F1E1E] text-center'>Showcase Of Our <span className='text-[#CB2D3E] italic'>Achievements</span></h3>
+                <div className="pt-21.25">
+                    <Flex className={'justify-center gap-x-68'}>
+                        <div className="text-center">
+                            <h3 className='text-[#F5595A] font-bold text-[72px] '><SlotCounter value={57} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }} /></h3>
+                            <p className='text-black font-medium text-[24px]'>Clients</p>
+                        </div>
+                        <div className="text-center">
+                            <h3 className='text-[#F5595A] font-bold text-[72px] '><SlotCounter value={93} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }} /></h3>
+                            <p className='text-black font-medium text-[24px]'>Projects</p>
+                        </div>
+                        <div className="text-center">
+                            <h3 className='text-[#F5595A] font-bold text-[72px] '><SlotCounter value={600} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }} /></h3>
+                            <p className='text-black font-medium text-[24px]'>Employees </p>
+                        </div>
+                        <div className="text-center">
+                            <h3 className='text-[#F5595A] font-bold text-[72px] '><SlotCounter value={25} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }} /></h3>
+                            <p className='text-black font-medium text-[24px]'>Awards</p>
+                        </div>
+                    </Flex>
+                </div>
+            </Container>
         </section>
         </>
     )
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Banner from "./layouts/Banner";
+import Achievement from "./layouts/Achievement";
 
 export default function Home() {
   return (
     <>
     <Banner/>
+    <Achievement/>
     </>
   );
 }
