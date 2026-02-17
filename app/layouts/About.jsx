ff1ec22d86aed205efc94b3a8a3dbda4d762f1d7
Sijan666 <666majharulislam@gmail.com>
2026-02-18 02:08:17 +0600

    feat : add about section

---
+++ app/layouts/About.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Container from '../[components]/Container'
+import Flex from '../[components]/Flex'
+import Image from 'next/image'
+
+const About = () => {
+    return (
+        <>
+        <section>
+            <div className="bg-[#F7FCE7] p-15">
+                <Container>
+                    <Flex className={'justify-between'}>
+                        <div className="left w-160.75">
+                            <h4 className='text-[32px] text-[#EF473A] font-bold italic pb-2'>We started back in 2008.</h4>
+                            <h3 className='text-[40px] text-[#342B27] font-bold'>Since then, we have been travelling towards one goal creating a supreme experience for our clients.</h3>
+                            <p className='text-[#605F62] text-base pt-7.5 pb-15'>We have designed our courses with the most demanding professional skills. The knowledge, experience, and expertise gained through the program will ensure your desired job in the global market.</p>
+                            <Flex className={'gap-x-5'}>
+                                <button className='text-white bg-[#FF1E1E] rounded-[41.5px] text-[18px] font-medium px-8 py-4 hover:text-[#FF1E1E] hover:bg-white duration-300 cursor-pointer'>Get Started </button>
+                                <button className='text-white bg-[#FF1E1E] rounded-[41.5px] text-[18px] font-medium px-8 py-4 hover:text-[#FF1E1E] hover:bg-white duration-300 cursor-pointer'>Explore more </button>
+                            </Flex>
+                        </div>
+                        <div className="right">
+                            <Image src='/about.png' height={624} width={628}/>
+                        </div>
+                    </Flex>
+                </Container>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default About
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Banner from "./layouts/Banner";
 import Achievement from "./layouts/Achievement";
+import About from "./layouts/About";
 
 export default function Home() {
   return (
     <>
     <Banner/>
     <Achievement/>
+    <About/>
     </>
   );
 }
