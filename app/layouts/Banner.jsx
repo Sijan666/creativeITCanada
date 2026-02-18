ef52f1204bfac66e5a2bfc2de5bbb30a8902d1f1
Sijan666 <666majharulislam@gmail.com>
2026-02-18 23:49:10 +0600

    add comenting and responsive banner

--- app/layouts/Banner.jsx
+++ app/layouts/Banner.jsx
@@ -5,17 +5,19 @@ import Flex from '../[components]/Flex'
 const Banner = () => {
     return (
         <section>
-            <div className="bg-[url('/bannerBg.png')] pt-48.25 pb-43 bg-center bg-cover bg-no-repeat">
-                <Container>
-                    <h3 className='text-[42px] font-semibold text-[#FF7E31] italic'>
+            <div className="bg-[url('/bannerBg.png')] lg:pt-48.25 lg:pb-43 py-20 bg-center bg-cover bg-no-repeat">
+                <Container className={'px-5'}>
+                    {/* banner text */}
+                    <h3 className='lg:text-[42px] text-base font-semibold text-[#FF7E31] italic pb-3 lg:pb-0'>
                         The Journey of
                     </h3>
-                    <h1 className='text-[92px] font-bold text-white max-w-142 leading-tight pb-25'>
+                    <h1 className='lg:text-[92px] text-base font-bold text-white lg:w-142 leading-tight lg:pb-25 pb-4'>
                         Creativity & Innovation
                     </h1>
-                    <Flex className={'gap-x-5'}>
-                        <button className='text-white bg-[#FF1E1E] rounded-[41.5px] text-[18px] font-medium px-8 py-4 hover:text-[#FF1E1E] hover:bg-white duration-300 cursor-pointer'>Get Started </button>
-                        <button className='text-white bg-[#FF1E1E] rounded-[41.5px] text-[18px] font-medium px-8 py-4 hover:text-[#FF1E1E] hover:bg-white duration-300 cursor-pointer'>Explore more </button>
+                    {/* btn */}
+                    <Flex className={'lg:gap-x-5 gap-x-1'}>
+                        <button className='text-white bg-[#FF1E1E] rounded-[41.5px] lg:text-[18px] text-sm font-medium lg:px-8 px-4 lg:py-4 py-2 hover:text-[#FF1E1E] hover:bg-white duration-300 cursor-pointer'>Get Started </button>
+                        <button className='text-white bg-[#FF1E1E] rounded-[41.5px] lg:text-[18px] text-sm font-medium lg:px-8 px-4 lg:py-4 py-2 hover:text-[#FF1E1E] hover:bg-white duration-300 cursor-pointer'>Explore more </button>
                     </Flex>
                 </Container>
             </div>
