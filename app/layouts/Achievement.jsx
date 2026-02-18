d60d68289f214c51a1ed9cc71032dd7a5913dd5f
Sijan666 <666majharulislam@gmail.com>
2026-02-18 23:51:56 +0600

    add comenting and responsuve achievement part

--- app/layouts/Achievement.jsx
+++ app/layouts/Achievement.jsx
@@ -8,10 +8,11 @@ const Achievement = () => {
     return (
         <>
         <section className='py-25'>
-            <Container>
-                <h3 className='text-[48px] font-bold text-[#1F1E1E] text-center'>Showcase Of Our <span className='text-[#CB2D3E] italic'>Achievements</span></h3>
-                <div className="pt-21.25">
-                    <Flex className={'justify-center gap-x-68'}>
+            <Container className={'px-5'}>
+                <h3 className='lg:text-[48px] text-[30px] font-bold text-[#1F1E1E] text-center'>Showcase Of Our <span className='text-[#CB2D3E] italic'>Achievements</span></h3>
+                <div className="lg:pt-21.25 pt-10">
+                    {/* counter part */}
+                    <Flex className={'lg:justify-center lg:gap-x-68 flex flex-col lg:flex-row gap-y-5'}>
                         <div className="text-center">
                             <h3 className='text-[#F5595A] font-bold text-[72px] '><SlotCounter value={57} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }} /></h3>
                             <p className='text-black font-medium text-[24px]'>Clients</p>
